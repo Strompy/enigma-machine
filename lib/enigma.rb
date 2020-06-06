1a5f1d2692fe898e034e8c7f7fbf78c5d0dd3e45
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 15:33:23 -0600

    Build create_key method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,7 +3,7 @@ require 'Date'
 class Enigma
   attr_reader :file, :shift, :date, :char_array
 
-  def initialize(file, shift = shift, date = Enigma.format_date)
+  def initialize(file, shift = nil, date = Enigma.format_date)
     @file = file
     @shift = shift
     @date = date
@@ -13,5 +13,9 @@ class Enigma
   def self.format_date
     test = Date.today
     test.strftime('%d%m%Y')
+  end
+
+  def create_key
+    key = rand(100_000).to_s.rjust(5, '0')
   end
 end
