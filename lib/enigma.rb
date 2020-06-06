1fb187fa5f51fa18ea018bd7db645fabf3c22477
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 15:11:56 -0600

    Add character array creation

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,13 @@
 require 'Date'
 
 class Enigma
-  attr_reader :file, :shift, :date
+  attr_reader :file, :shift, :date, :char_array
 
   def initialize(file, shift = shift, date = Enigma.format_date)
     @file = file
     @shift = shift
     @date = date
+    @char_array = ("a".."z").to_a << " "
   end
 
   def self.format_date
