deddd886b779532d6016fc376ab07bd0466dfad5
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 14:53:11 -0600

    Create existence test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,4 +3,10 @@ require 'minitest/pride'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    file = File.open('./message.txt', 'r')
+    enigma = Enigma.new(file)
+    assert_instance_of Enigma , enigma
+  end
 end
