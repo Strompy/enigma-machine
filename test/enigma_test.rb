05ea82b49862eb12c79fdf7a005036049a5dcc03
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 14:55:55 -0600

    Create attributes test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,4 +9,12 @@ class EnigmaTest < Minitest::Test
     enigma = Enigma.new(file)
     assert_instance_of Enigma , enigma
   end
+
+  def test_it_has_attributes
+    file = File.open('./message.txt', 'r')
+    enigma = Enigma.new(file, 01234)
+    assert_equal file, enigma.file
+    assert_equal 01234, enigma.shift
+    assert_equal Date.today, enigma.date
+  end
 end
