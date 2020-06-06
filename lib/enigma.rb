63350b0595c0b9e2895d5f5ca3f499714ed03638
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 15:09:31 -0600

    Create format date method test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,16 @@
 require 'Date'
 
 class Enigma
-attr_reader :file, :shift, :date
-def initialize(file, shift = shift, date = Date.today)
-  @file = file
-  @shift = shift
-  @date = date
-end
+  attr_reader :file, :shift, :date
+
+  def initialize(file, shift = shift, date = Enigma.format_date)
+    @file = file
+    @shift = shift
+    @date = date
+  end
+
+  def self.format_date
+    test = Date.today
+    test.strftime('%d%m%Y')
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma'
+require 'mocha/minitest'
 
 class EnigmaTest < Minitest::Test
 
@@ -12,9 +13,16 @@ class EnigmaTest < Minitest::Test
 
   def test_it_has_attributes
     file = File.open('./message.txt', 'r')
-    enigma = Enigma.new(file, 01234)
+    enigma = Enigma.new(file, 01234, Date.today)
     assert_equal file, enigma.file
     assert_equal 01234, enigma.shift
     assert_equal Date.today, enigma.date
+  end
+
+  def test_it_can_format_date
+    file = File.open('./message.txt', 'r')
+    enigma = Enigma.new(file, 01234)
+    Date.stubs(:today).returns(Date.new(2020, 06, 06))
+    assert_equal '06062020', enigma.date
   end
 end
