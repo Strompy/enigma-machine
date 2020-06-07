d5134f529fbbf98a4e10fab1b0cfc352566fecfd
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 18:11:23 -0600

    Create key_generator test

---
+++ test/key_generator_test.rb
@@ -0,0 +1,8 @@
+require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/key_generator'
+require 'mocha/minitest'
+
+class KeyGeneratorTest < Minitest::Test
+end
