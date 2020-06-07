91b9d16e4725a1c8557bed9eafaf1b6dd026599a
Phillip Strom <phillip.strom@gmail.com>
2020-06-07 11:52:11 -0600

    Create shift array helper method

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,9 +1,11 @@
 require './lib/offset_generator'
 
 class ShiftGenerator < OffsetGenerator
-
+  attr_reader :shifts
   def initialize
     @shift = {}
+    @char_array = ("a".."z").to_a << " "
+    @shifts = []
     super
   end
 
@@ -11,6 +13,12 @@ class ShiftGenerator < OffsetGenerator
     self.create_key_hash
     @shift = key_hash.merge(offset_hash) do |symbol, key_value, offset_value|
       key_value.to_i + offset_value.to_i
+    end
+  end
+
+  def create_shift_hashes
+    @shift.map do |symbol, shift|
+      @shifts << @char_array.zip(@char_array.rotate(shift)).to_h
     end
   end
 
