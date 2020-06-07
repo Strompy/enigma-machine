1088e12b33ea8e41228026d94c3c71967b8da9a2
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 18:32:10 -0600

    Create key_hash test

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -14,28 +14,31 @@ class KeyGeneratorTest < Minitest::Test
   def test_it_has_attributes
     key_gen = KeyGenerator.new
     expected = {}
-    assert_equal expected, key_gen.key
+    assert_equal '', key_gen.key
     assert_equal expected, key_gen.key_hash
   end
 
   def test_it_can_create_key
-    KeyGenerator.stubs(:rand).returns(1234)
-    enigma = KeyGenerator.new
+    key_gen = KeyGenerator.new
+    key_gen.stubs(:rand).returns(1234)
     expected_key = "01234"
-    assert_equal 5, enigma.key.length
-    assert_equal expected_key, enigma.key
+    key_gen.create_key
+    assert_equal 5, key_gen.key.length
+    assert_equal expected_key, key_gen.key
   end
 
   def test_it_can_make_key_hash
-    KeyGenerator.stubs(:rand).returns(1234)
-    enigma = KeyGenerator.new
+    key_gen = KeyGenerator.new
+    key_gen.stubs(:rand).returns(1234)
+    key_gen.create_key
+    key_gen.create_key_hash
     expected = {:A => "01",
     :B => "12",
     :C => "23",
     :D => "34"
     }
 
-    assert_equal expected, enigma.key_hash
+    assert_equal expected, key_gen.key_hash
   end
 
 end
