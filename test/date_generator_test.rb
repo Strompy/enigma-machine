deb09ae3771251cebed7fb6976b95b6b8a23f8d1
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 20:36:47 -0600

    Create date test

---
+++ test/date_generator_test.rb
@@ -0,0 +1,9 @@
+require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/date_generator'
+require 'mocha/minitest'
+
+class KeyGeneratorTest < Minitest::Test
+
+end
