bc8a23f863d801ce93711bb78736e1110d2a8b20
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 21:25:27 -0600

    Refactoring Enigma Class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,11 @@
 require 'Date'
+require './lib/shift_generator'
 
 class Enigma < ShiftGenerator
   attr_reader :char_array, :message, :key, :date, :shift
 
   def initialize(message, key = KeyGenerator.new.create_key, date = OffsetGenerator.new.date)
-    super
+    # super
     @char_array = ("a".."z").to_a << " "
     @message = message
     @key = key
