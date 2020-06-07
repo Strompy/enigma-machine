54a0107792eafc7f974e3468fe78025626afdad4
Phillip Strom <phillip.strom@gmail.com>
2020-06-06 18:12:33 -0600

    Create existence test

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -5,4 +5,9 @@ require './lib/key_generator'
 require 'mocha/minitest'
 
 class KeyGeneratorTest < Minitest::Test
+
+  def test_it_exists
+    key_gen = KeyGenerator.new
+    assert_instance_of KeyGenerator, key_gen
+  end
 end
