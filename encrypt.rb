ecebd0b9da286861d13d620b903ba445d13e29ee
Phillip Strom <phillip.strom@gmail.com>
2020-06-07 16:05:09 -0600

    Create encryption runner

---
+++ encrypt.rb
@@ -0,0 +1,14 @@
+require './lib/enigma.rb'
+
+reader = File.open(ARGV[0], 'r')
+incoming_text = handle.read
+reader.close
+
+encryptor = Enigma.new(incoming_text)
+encryption = encryptor.encrypt
+
+writer = File.open(ARGV[1], 'w')
+writer.write(encryption)
+writer.close
+
+puts "Created '#{ARGV[1]}' with key: #{encryption[:key]} and date: #{encryption[:date]}"
